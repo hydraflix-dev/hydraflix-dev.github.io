125ab98e80f9e9f864e89a4ce8641414080180e7
Elio Osés <eliooses@netfilmx.com>
2016-04-21 01:35:01 -0300

    Update CORS proxy, testing "alloworigin"

--- js/search.js
+++ js/search.js
@@ -26,12 +26,12 @@ $('#submit').pressEnter(function(){
 	return false;
 });
 var search_parameter = getUrlParameter('q');
-$.getJSON("http://alloworigin.com/get?url=https://yts.ag/api/v2/list_movies.json?query_term="+search_parameter, function (data) { // http://crossorigin.me/https://yts.ag/api/v2/list_movies.json?query_term="+search_parameter, function (data) {
+$.getJSON("http://alloworigin.com/get?url=http://yify.is/index.php/api/v2/list_movies.json?query_term="+search_parameter, function (data) { // http://crossorigin.me/https://yts.ag/api/v2/list_movies.json?query_term="+search_parameter, function (data) {
 	$('#movies').html("");
 
 	i = 0;
 		var trackers = "&tr=udp:\/\/open.demonii.com:1337\/announce&tr=http:\/\/tracker.trackerfix.com\/announce&tr=udp:\/\/9.rarbg.to:2710&tr=udp:\/\/9.rarbg.me:2710&tr=udp:\/\/exodus.desync.com:6969&tr=udp:\/\/tracker.coppersurfer.tk:6969&tr=udp:\/\/tracker.leechers-paradise.org:6969&tr=udp:\/\/tracker.openbittorrent.com:80&tr=udp:\/\/glotorrents.pw:6969\/announce&tr=udp:\/\/tracker.opentrackr.org:1337\/announce&tr=udp:\/\/torrent.gresille.org:80\/announce&tr=udp:\/\/p4p.arenabg.com:1337&tr=udp:\/\/tracker.internetwarriors.net:1337&tr=http:\/\/www.siambt.com\/announce.php&tr=http:\/\/bttracker.crunchbanglinux.org:6969\/announce&tr=http:\/\/www.eddie4.nl:6969\/announce&tr=http:\/\/mgtracker.org:2710\/announce";
-	json_data = data.data.movies;
+	json_data = data.contents.data.movies;
 
 	// JSON DATA
 	$.each(json_data, function (i, item) {
