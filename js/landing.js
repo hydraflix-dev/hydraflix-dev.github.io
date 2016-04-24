b2c3c06d5706e7def24b7ff7358f69e52615a77a
Elio Osés <eliooses@netfilmx.com>
2016-04-24 01:02:21 -0300

    Update landing.js

--- js/landing.js
+++ js/landing.js
@@ -33,8 +33,8 @@ function onPlayerStateChange(e) {
 
 function vidRescale(){
 
-  var w = $(window).width(),
-    h = $(window).height()/2;
+  var w = $(window).width()+200,
+    h = $(window).height()+200;
 
   if (w/h > 16/9){
     tv.setSize(w, w/16*9);
