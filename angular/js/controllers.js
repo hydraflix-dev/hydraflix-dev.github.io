41a511a6e8055831ac953e94f1b64a60d3a3f5e9
Elio Osés <eliooses@netfilmx.com>
2016-07-30 04:16:38 -0300

    Add files via upload

---
+++ angular/js/controllers.js
@@ -0,0 +1,53 @@
+var playflix = angular.module('playflix',[]);
+
+playflix.controller('navbarController', ['$scope', function ($scope) {
+    $scope.navbars = [{
+        name: 'navbar.html',
+        url: 'inc/navbar.html'}];
+        
+    $scope.navbar = $scope.navbars[0];
+}]);
+/*
+playflix.controller('headController', ['$scope', function ($scope) {
+    $scope.heads = [{
+        name: 'head.html',
+        url: 'inc/head.html'}];
+        
+    $scope.head = $scope.heads[0];
+}]);
+playflix.controller('moviesController', ['$scope', function ($scope) {
+    $scope.movies = [{
+        name: 'movie.html',
+        url: 'inc/movie.html'}];
+        
+    $scope.movie = $scope.movies[0];
+}]);
+playflix.controller('preloaderController', ['$scope', function ($scope) {
+    $scope.preloaders = [{
+        name: 'preloader.html',
+        url: 'inc/preloader.html'}];
+        
+    $scope.preloader = $scope.preloaders[0];
+}]);
+playflix.controller('catalogueController', ['$scope', function ($scope) {
+    $scope.catalogues = [{
+        name: 'catalogue.html',
+        url: 'inc/catalogue.html'}];
+        
+    $scope.catalogue = $scope.catalogues[0];
+}]);
+playflix.controller('detailController', ['$scope', function ($scope) {
+    $scope.details = [{
+        name: 'detail.html',
+        url: 'inc/detail.html'}];
+        
+    $scope.detail = $scope.details[0];
+}]);
+playflix.controller('jsController', ['$scope', function ($scope) {
+    $scope.jss = [{
+        name: 'js.html',
+        url: 'inc/js.html'}];
+        
+    $scope.js = $scope.jss[0];
+}]);
+*/
