809c1c7b23421427a107aff3728e085c365feeae
Elio Osés <eliooses@netfilmx.com>
2016-08-05 02:11:18 -0300

    Update services.js

--- angular/v2/js/services.js
+++ angular/v2/js/services.js
@@ -1,24 +1,10 @@
-var yify = "http://crossorigin.me/http://yify.is/index.php/api/v2/list_movies.json";
-angular.module('myAppServices', ['ngResource']);
 
-    factory('BreakinBadChar', function($resource){
-
-  return $resource(yify, {}, {
-
-    query: {method:'GET', isArray: true}
-
-  });
-
-});
-    /*
 
 function InstantSearchController($scope, $http){
  $scope.search = function() {   
-  $http.jsonp("http://something.com/lol?query="+ $scope.searchString + "?json_callback=JSON_CALLBACK").success(
+  $http.jsonp("http://yify.is/index.php/api/v2/list_movies.json").success(
                         function(data, status) {
                             console.log(data);
                         }
                 );
  }
-
-    */
