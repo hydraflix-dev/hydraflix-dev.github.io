592b9e0c3c41a94213036094852311477cd1f410
Elio Osés <eliooses@netfilmx.com>
2016-08-05 01:54:49 -0300

    Update services.js

--- angular/v2/js/services.js
+++ angular/v2/js/services.js
@@ -1,10 +1,11 @@
+var yify = "https://yts.ag/api/v2/list_movies.json"
 angular.module('myAppServices', ['ngResource']).
 
     factory('BreakinBadChar', function($resource){
 
-  return $resource(':resourceName.json', {}, {
+  return $resource(resourceName, {}, {
 
-    query: {method:'GET', params:{resourceName:'breaking_bad_char'}, isArray: true}
+    query: {method:'GET', params:{resourceName:yify}, isArray: true}
 
   });
 
