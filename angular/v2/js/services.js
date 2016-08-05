15335144e1f8b2199162ca84ecf20c4618702355
Elio Osés <eliooses@netfilmx.com>
2016-08-05 02:15:03 -0300

    Update services.js

--- angular/v2/js/services.js
+++ angular/v2/js/services.js
@@ -1,8 +1,9 @@
-function InstantSearchController($scope, $http){
- $scope.search = function() {   
-  $http.jsonp("http://yify.is/index.php/api/v2/list_movies.json").success(
-           function(data, status) {
-               console.log(data);
-           }
-   );
+function InstantSearchController($scope, $http) {
+	$scope.search = function() {   
+	$http.jsonp("http://yify.is/index.php/api/v2/list_movies.json").success(
+            function(data, status) {
+                console.log(data);
+            }
+        );
+	}
  };
