3c823f1c147f42978c5c379457434f884e1a0104
Elio Osés <eliooses@netfilmx.com>
2016-08-05 01:56:25 -0300

    Update services.js

--- angular/v2/js/services.js
+++ angular/v2/js/services.js
@@ -1,4 +1,4 @@
-var yify = "https://yts.ag/api/v2/list_movies.json"
+var yify = "http://crossorigin.me/https://yts.ag/api/v2/list_movies.json"
 angular.module('myAppServices', ['ngResource']).
 
     factory('BreakinBadChar', function($resource){
