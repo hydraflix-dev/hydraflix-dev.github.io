155b3ac7ed469729a129e91e659a7c548ecc9c97
Elio Osés <eliooses@netfilmx.com>
2016-08-07 07:51:47 -0300

    Add files via upload

---
+++ playflix-v3/js/app.js
@@ -0,0 +1,27 @@
+var myApp = angular.module('myApp', ['infinite-scroll']);
+
+myApp.controller('DemoController', function($scope, Playflix) {
+  $scope.playflix = new Playflix();
+});
+// Playflix constructor function to encapsulate HTTP and pagination logic
+myApp.factory('Playflix', function($http) {
+  var page = 1;
+  var Playflix = function() {
+    this.items = [];
+    this.busy = false;
+  };
+  Playflix.prototype.nextPage = function() {
+    if (this.busy) return;
+    this.busy = true;    
+    $http.get("https://yts.ag/api/v2/list_movies.jsonp?sort_by=year&quality=720p&limit=24&page="+page).success(function(data) {
+      var items = data.data.movies;
+      var i = 0;
+      for (var i = 0; i < items.length; i++) {
+        this.items.push(items[i]);
+      }
+      this.busy = false;
+    }.bind(this));
+    page = page + 1;
+  };
+  return Playflix;
+});
---
+++ playflix-v3/js/catalogue-aux.js
@@ -0,0 +1,20 @@
+			$(function() { // DOM ready
+			
+			  var txt = ['movies', 'tv shows', 'documentaries', 'concerts'],
+			      n = txt.length,
+			      $swap = $('#swap'),
+			      $span,
+			      c = -1;
+			
+			  // CREATE SPANS INSIDE SPAN
+			  for(var i=0; i<txt.length; i++) $swap.append($('<span />',{text:txt[i]}));
+			  // HIDE AND COLLECT THEM
+			  $span = $("span", $swap).hide(); 
+			
+			  (function loop(){
+			    c = ++c % n;
+			    $swap.animate({width: $span.eq( c ).width() });
+			    $span.stop().fadeOut().eq(c).fadeIn().delay(1000).show(0, loop);
+			  }()); 
+			
+			});	
